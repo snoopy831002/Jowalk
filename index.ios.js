4544e02b6432abee8454a68468a26e163048b1d9
snoopy831002 <snoopy831002@gmail.com>
2017-03-02 21:48:03 +0800

    Add Modal and onMove distance detection

--- index.ios.js
+++ index.ios.js
@@ -12,10 +12,10 @@ import {
   Dimensions,
   TouchableOpacity,
   Text,
+  Modal,
   View
 } from 'react-native';
 import Sketch from 'react-native-sketch';
-var TimerMixin = require('react-timer-mixin');
 
 export default class Jowalk039 extends Component {
    constructor(props) {
@@ -24,14 +24,32 @@ export default class Jowalk039 extends Component {
     this.onReset = this.onReset.bind(this);
     this.onSave = this.onSave.bind(this);
     this.onUpdate = this.onUpdate.bind(this);
-    var EndXcoordinate;
-    var EndYcoordinate;
-    var StartXcoordinate;
-    var StartYcoordinate;
+    global.flag = false;
+    global.Interval;
+    global.coordinates = {StartXcoordinate: '', EndXcoordinate: '',StartYcoordinate: '',EndYcoordinate: ''};
+    global.distanceArr = [];
   }
 
   state = {
     encodedSignature: null,
+    //
+    animationType: 'fade',
+    modalVisible: false,
+    transparent: false,
+    selectedSupportedOrientation: 1,
+    //
+  };
+
+  _setModalVisible = (visible) => {
+    this.setState({modalVisible: visible});
+  };
+
+  _setAnimationType = (type) => {
+    this.setState({animationType: type});
+  };
+
+  _toggleTransparent = () => {
+    this.setState({transparent: !this.state.transparent});
   };
 
   /**
@@ -69,26 +87,75 @@ export default class Jowalk039 extends Component {
     this.setState({ encodedSignature: base64Image });
   }
 
-  handlePressIn(evt){
-    //console.log(`start x coord = ${evt.nativeEvent.locationX}`);
-    console.log('in');
-     this.timer = setInterval(
-      () => { console.log('把一个定时器的引用挂在this上'); },
-      50
-    );
+  _onMoveShouldSetResponder(e) {
+    return true;
   }
 
-  handlePressOut(evt){
-    //console.log(`start x coord = ${evt.nativeEvent.locationX}`);
-    console.log('out');
-      this.timer && clearTimeout(this.timer);
+  _onStartShouldSetResponder(e) {
+    return true;
+  }
+  //
+
+  handlePressIn(e){
+    coordinates.StartXcoordinate = e.nativeEvent.locationX ;
+    coordinates.StartYcoordinate = e.nativeEvent.locationY ;
+    Interval = setInterval(() => { flag = true; }, 1000);
+  }
+
+  handlePressOut(){
+    clearInterval(Interval);
+    console.log(distanceArr);
+  }
+
+  handleOnMove(e) {
+    if(flag == true) {
+      coordinates.EndXcoordinate = e.nativeEvent.locationX;
+      coordinates.EndYcoordinate = e.nativeEvent.locationY;
+      var distance = calculateDistance(coordinates.StartXcoordinate,coordinates.EndXcoordinate,coordinates.StartYcoordinate,coordinates.EndYcoordinate);
+      distanceArr.push(distance);
+      coordinates.StartXcoordinate = coordinates.EndXcoordinate ;
+      coordinates.StartYcoordinate = coordinates.EndYcoordinate ;
+      flag = false ;
+    }
   }
 
   render() {
+    var modalBackgroundStyle = {
+      backgroundColor: this.state.transparent ? 'rgba(0, 0, 0, 0.5)' : '#f5fcff',
+    };
+    var innerContainerTransparentStyle = this.state.transparent
+      ? {backgroundColor: '#fff', padding: 20}
+      : null;
+    var activeButtonStyle = {
+      backgroundColor: '#ddd'
+    };
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPressIn={(evt) => this.handlePressIn(evt) } onPressOut={(evt) => this.handlePressOut(evt) } > 
+        <Modal
+          animationType={this.state.animationType}
+          transparent={this.state.transparent}
+          visible={this.state.modalVisible}
+          onRequestClose={() => this._setModalVisible(false)}
+          supportedOrientations={['landscape']}
+          >
+          <View style={[styles.modalContainer, modalBackgroundStyle]}>
+            <View style={[styles.innerContainer, innerContainerTransparentStyle]}>
+              <Text>AAAAAAAAAAAAAAA</Text>
+              <Text>BBBBB</Text>
+              <Button
+                title="Close"
+                onPress={this._setModalVisible.bind(this, false)}
+                style={styles.modalButton}>
+              </Button>
+            </View>
+          </View>
+        </Modal>
           <Sketch
+            onStartShouldSetResponder={this._onStartShouldSetResponder}
+            onMoveShouldSetResponder={this._onMoveShouldSetResponder}
+            onResponderStart={this.handlePressIn}
+            onResponderMove={this.handleOnMove}
+            onResponderRelease={this.handlePressOut}
             fillColor="#f5f5f5"
             strokeColor="#111111"
             strokeThickness={2}
@@ -97,10 +164,13 @@ export default class Jowalk039 extends Component {
             ref={(sketch) => { this.sketch = sketch; }}
             style={styles.sketch}
           />
-        </TouchableOpacity>
         <Button
           onPress={this.clear}
           title="Clear drawing"
+        />
+        <Button 
+          onPress={this._setModalVisible.bind(this, true)}
+          title="Present"
         />
         <Button
           disabled={!this.state.encodedSignature}
@@ -113,13 +183,27 @@ export default class Jowalk039 extends Component {
   
 }
 
+function calculateDistance(InX,OutX,InY,OutY){
+  return Math.sqrt(Math.pow(OutX-InX,2)+Math.pow(InY-OutY,2));
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
   sketch: {
     flexGrow: 1,
     height: Dimensions.get("window").height-100, // Height needed; Default: 200px
+  },
+  innerContainer: {
+    borderRadius: 10,
+    backgroundColor: 'blue',
+    alignItems: 'center',
   },
 });
 
